57e65ebaa04b2ecd2732d779d5f424823f1df39c
insertrandomcode <insertrandomcode@Polaris>
2024-04-30 14:27:01 +0200

    expanded skeleton

--- bij_encodings/cons_propagator/atleast1bij_constraint.py
+++ bij_encodings/cons_propagator/atleast1bij_constraint.py
@@ -1,4 +1,7 @@
+
 from typing import List, Dict
+from functools import reduce
+from collections import defaultdict
 
 from bij_encodings.cons_propagator.singlebij_constraint import ConsBijConstraint
 
@@ -7,15 +10,34 @@ class ConsAtLeast1BijConstraint():
         
         self.cons = bijconstraints
 
-        pass
+        self.watchlist = defaultdict(lambda : [])
+
+        for cs in self.cons:
+            for var in cs.vset:
+                self.watchlist[var].append(cs)
+
+        self.vset = set(self.watchlist.keys())
+
+        self.assignment = None
 
     def attach_values(self, values) -> None:
-        pass
+
+        self.assignment = values
+
+        for cs in self.cons:
+            cs.attach_values(values)
 
     def register_watched(self, to_watch) -> None:
-        pass
+        for var in self.vset:
+            to_watch[var].append(self)
+        
 
     def propagate(self, lit: int = None) -> List[int]:
+        """
+        Need at least 1 value potential bijection to be true.
+
+        Propagate when only 1 option left accross all bijconstraints
+        """
         pass
 
     def justify(self, lit: int) -> List[int]:
