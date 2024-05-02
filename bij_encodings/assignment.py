11b5b83f5d9e4c3bdb95c150adbfdb7b9b74c5bd
insertrandomcode <paramihijoalejandro@gmail.com>
2024-05-02 10:40:45 +0200

    generalised Assignment class to work for tuples of any size

--- bij_encodings/assignment.py
+++ bij_encodings/assignment.py
@@ -1,22 +1,31 @@
 from typing import Tuple
 
 class Assignment():
-        def __init__(self, offset = 0):
+        def __init__(self, offset = 0, assignees: int = 2):
             self.assignment = {}
             self.inv_assignment = [None]
             self.curr = 1 + offset
             self.offset = offset
         
-        def get_assignment(self, i: int, j: int) -> int:
+        def get_assignment(self, *args) -> int:
             ## assignment i, j is from S1 to S2
-            try:
-                return self.assignment[i][j]
-            except KeyError:
-                self.assignment.setdefault(i, {})
-                self.assignment[i][j] = self.curr
-                self.inv_assignment.append((i, j))
+            curr = self.assignment
+
+            for arg in args[:-1]:
+                curr = curr.setdefault(arg, {})
+            
+            res = curr.setdefault(args[-1], None)
+
+
+            if res == None:
+                # set value
+                curr[args[-1]] = self.curr
+                self.inv_assignment.append(args)
                 self.curr += 1
-                return self.assignment[i][j]
+                return curr[args[-1]]
+            else:
+                # is int
+                return res
         
         def get_inv_assignment(self, i: int) -> Tuple[int, int]:
             assert i -self.offset > 0, "invalid choice for i"
