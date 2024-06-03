c89c034f38fafd1dbb2f48399fdf30a72a451087
insertrandomcode <paramihijoalejandro@gmail.com>
2024-06-03 12:30:49 +0200

    implented graph class

--- graph_testing.py
+++ graph_testing.py
@@ -3,13 +3,17 @@ if __name__ == '__main__':
     from r1cs_scripts.read_r1cs import parse_r1cs
     from compare_circuits import get_classes
     from structural_analysis.signal_graph import *
+    from structural_analysis.constraint_graph import *
 
     circ = Circuit()
     
-    #filename = "SudokuO1.r1cs"
-    filename = "Poseidon.r1cs"
+    # filename = "r1cs_files/SudokuO1.r1cs"
+    filename = "r1cs_files/Poseidon.r1cs"
+    # filename = "r1cs_files/MultiAND.r1cs"
 
-    graph_gen = negone_to_signal
+    # graph_gen = abc_signal_graph    
+    # graph_gen = negone_to_signal
+    graph_gen = shared_signal_graph
     
     parse_r1cs(filename, circ)
     groups = get_classes(circ, circ, [("S1", circ), ("S2", circ)])
@@ -18,7 +22,6 @@ if __name__ == '__main__':
         circ.constraints,
     )
 
-    write_png(
-        g,
-        "structural_analysis/" + filename + ".png"
+    g.write_png(
+        "structural_analysis" + filename[filename.index('/'):] + ".png"
     )  
