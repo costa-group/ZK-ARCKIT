e18c2f7f73abe21ab727a39f29c314aaef7400f6
insertrandomcode <paramihijoalejandro@gmail.com>
2024-06-07 12:06:39 +0200

    implemented HCS using networkx

--- structural_analysis/graph_clustering/HCS_clustering.py
+++ structural_analysis/graph_clustering/HCS_clustering.py
@@ -1,1 +1,19 @@
+import itertools
+import networkx as nx
 
+
+def HCS(G: nx.Graph) -> nx.Graph:
+
+    cut = nx.algorithms.connectivity.minimum_edge_cut(G)
+
+    if len(cut) > G.number_of_nodes() // 2:
+        return G
+    else:
+        G_ = G.copy()
+        G_.remove_edges_from(cut)
+
+        # min-cut always returns 2 connected components
+        subgraphs = [G_.subgraph(component) for component in nx.connected_components(G_)]
+        subgraphs = [HCS(H) for H in subgraphs]
+
+        return nx.compose(*subgraphs)
