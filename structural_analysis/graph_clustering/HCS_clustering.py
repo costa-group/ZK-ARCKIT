1e0e57b247c18ff567959137f1b693de2d5559f6
insertrandomcode <paramihijoalejandro@gmail.com>
2024-06-07 12:42:05 +0200

    added nonrecursive version to avoid recursion limit errors

--- structural_analysis/graph_clustering/HCS_clustering.py
+++ structural_analysis/graph_clustering/HCS_clustering.py
@@ -1,12 +1,10 @@
 import itertools
 import networkx as nx
 
-
-def HCS(G: nx.Graph) -> nx.Graph:
-
+def HCS_recursion(G: nx.Graph) -> nx.Graph:
     cut = nx.algorithms.connectivity.minimum_edge_cut(G)
 
-    if len(cut) > G.number_of_nodes() // 2:
+    if len(cut) == 0 or len(cut) > G.number_of_nodes() // 2:
         return G
     else:
         G_ = G.copy()
@@ -14,6 +12,30 @@ def HCS(G: nx.Graph) -> nx.Graph:
 
         # min-cut always returns 2 connected components
         subgraphs = [G_.subgraph(component) for component in nx.connected_components(G_)]
-        subgraphs = [HCS(H) for H in subgraphs]
+        subgraphs = [HCS_recursion(H) for H in subgraphs]
 
         return nx.compose(*subgraphs)
+
+def HCS_norecursion(G: nx.Graph) -> nx.Graph:
+    G_ = G.copy()
+
+    stack = [G_.nodes()]
+
+    while len(stack) > 0:
+
+        nodes = stack.pop()
+        H = G_.subgraph(nodes)
+
+        cut = nx.algorithms.connectivity.minimum_edge_cut(H)
+
+        if len(cut) > 0 and len(cut) <= H.number_of_nodes() // 2:
+
+            G_.remove_edges_from(cut)
+            H = G_.subgraph(nodes)
+
+            stack += list(nx.connected_components(H))
+    
+    return G_
+
+def HCS(G: nx.Graph, recursion: bool = False):
+    return HCS_recursion(G) if recursion else HCS_norecursion(G)
