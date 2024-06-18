704e2d36b2f30a3f7a09c0bf73ca361201f8ecad
insertrandomcode <paramihijoalejandro@gmail.com>
2024-06-19 08:30:45 +1000

    added ability to conform to known signal bijections

--- bij_encodings/single_cons_options.py
+++ bij_encodings/single_cons_options.py
@@ -1,10 +1,12 @@
 from functools import reduce
 from itertools import chain
+from typing import Dict
 
 from r1cs_scripts.constraint import Constraint
 from bij_encodings.assignment import Assignment
 
-def signal_options(C1: Constraint, C2: Constraint, mapp: Assignment) -> dict:
+def signal_options(C1: Constraint, C2: Constraint, mapp: Assignment, 
+                   signal_bijection: Dict[str, Dict[int, int]] = None) -> dict:
     ## Assume input constraints are in a comparable canonical form
 
     # iterator for dicts in a constraint
@@ -60,6 +62,18 @@ def signal_options(C1: Constraint, C2: Constraint, mapp: Assignment) -> dict:
         }
         for name, i in [('S1', 0), ('S2', 1)]
     }
+
+    if signal_bijection is not None:
+        for name in ['S1', 'S2']:
+            for key in options[name].keys():
+                if key in signal_bijection[name].keys():
+
+                    ij = mapp.get_assignment(
+                        *((key, signal_bijection[name][key]) if name == "S1" else
+                        (signal_bijection[name][key], key))
+                    )
+
+                    options[name][key] = options[name][key].intersection([ij])
 
     # FINAL: for each circ -- for each signal - potential signals could map to
     #           intersection of potential mappings seen in each part         
