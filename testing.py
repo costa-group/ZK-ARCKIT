63e066a8364d1166a254ed856f609d736bd7c7d2
insertrandomcode <paramihijoalejandro@gmail.com>
2024-07-23 17:33:47 +0200

    removed redudant code on testing

--- testing.py
+++ testing.py
@@ -4,10 +4,12 @@ from pysat.card import CardEnc, EncType
 from pysat.solvers import Solver
 import pysat as ps
 import time 
+import numpy as np
 from functools import reduce
 
+from comparison.compare_circuits import circuit_equivalence
 from comparison_testing import get_circuits, shuffle_constraints
-from comparison.constraint_preprocessing import hash_constraint, constraint_classes
+from comparison.constraint_preprocessing import hash_constraint, constraint_classes, known_split
 from comparison.cluster_preprocessing import circuit_clusters, constraint_cluster_classes, groups_from_clusters
 from r1cs_scripts.modular_operations import multiplyP
 from r1cs_scripts.constraint import Constraint
@@ -19,11 +21,13 @@ from bij_encodings.reduced_encoding.red_class_encoder import reduced_encoding_cl
 from bij_encodings.reduced_encoding.red_natural_encoding import ReducedNaturalEncoder
 from bij_encodings.reduced_encoding.red_pseudoboolean_encoding import ReducedPseudobooleanEncoder, pseudoboolean_signal_encoder
 from bij_encodings.online_info_passing import OnlineInfoPassEncoder
-from bij_encodings.batched_info_passing import BatchedInfoPassEncoder
+from bij_encodings.batched_info_passing import BatchedInfoPassEncoder, recluster
 from structural_analysis.graph_clustering.degree_clustering import twice_average_degree, ratio_of_signals
 from structural_analysis.graph_clustering.signal_equivalence_clustering import naive_removal_clustering, is_signal_equivalence_constraint
 from structural_analysis.graph_clustering.clustering_from_list import cluster_from_list
 from comparison.static_distance_preprocessing import distances_to_static_preprocessing
+from testing_harness import run_affirmative_test
+from normalisation import r1cs_norm
 import itertools
 
 # REVEAL TEST TIMES (with singular preprocessing)
@@ -104,91 +108,32 @@ def getvars(con: Constraint) -> set:
     return set(con.A.keys()).union(con.B.keys()).union(con.C.keys()).difference(set([0]))
     
 if __name__ == '__main__':
-    filename = "r1cs_files/RevealO0.r1cs"
+    files = ["Poseidon", "Reveal", "Biomebase", "Move"]
+    optimisation = "O0"
 
-    circ, circs, mapp, cmapp = get_circuits(filename, seed = 42, 
-        const_factor=True, shuffle_sig=True, shuffle_const=True,
-        return_mapping=True, return_cmapping=True)
+    encoders = [OnlineInfoPassEncoder]
+    encoder_names = ["online_info"]
 
-    in_pair = [("S1", circ), ("S2", circs)]
+    RNG = np.random.default_rng(seed = 42)
 
-     # # NOTE: clustering working fine, removing the same constraints
+    for file in files:
+        for encoder, encoder_name in zip(encoders, encoder_names):
+            seed = int(RNG.integers(low = 1, high = 25565))
 
-    start = time.time()
-
-    mapp = Assignment()
-    assumptions = set([])
-    known_info = None
-
-    # TODO: look into why it's worse with more info? singular preprocessing performs .. worse? Doesn't seem possible
-    #       strictly better starting number of constraints leads to worse post-preprocessing clusters ???
-    known_info = distances_to_static_preprocessing(in_pair, assumptions, mapp)
-    
-    print("distances calc time: ", time.time() - start)
-
-    clusters = circuit_clusters(in_pair, naive_removal_clustering, calculate_adjacency = True)
-    classes = groups_from_clusters(in_pair, clusters, known_info, mapp)
-
-    # clusters = None
-    # classes = constraint_classes(in_pair)
-
-    post_classes = time.time()
-
-    print("grouping time: ",post_classes - start)
-
-    print("total num of constraint: ",sum(list(map(len, classes["S1"].values()))))
-    print("num_of_classes", count_ints(map(len, classes["S1"].values())))
-
-    # mapp = Assignment()
-    cmapp = Assignment(assignees = 3, link = mapp)
-    # assumptions = set([])
-    formula = CNF()
-
-    # TODO: let singular_class_preprocessing utilise clustering info
-    new_classes = classes
-    # new_classes, known_info = singular_class_preprocessing(
-    #     in_pair, classes, clusters,
-    #     mapp, cmapp, assumptions, formula, known_signal_info
-    # )
-
-    post_new_classes = time.time()
-
-    # print("singular preprocessing time: ",post_new_classes - post_classes)
-
-    # print("new total num of constraint: ",sum(list(map(len, new_classes["S1"].values()))))
-
-    # if len(new_classes["S1"].values()) > 0: print("num_of_classes", count_ints(map(len, new_classes["S1"].values())))
-
-    formula, assumptions = BatchedInfoPassEncoder().encode(
-        new_classes, in_pair, clusters, reduced_encoding_class, pseudoboolean_signal_encoder, False, False, True,
-        formula, mapp, cmapp, assumptions, known_info, batching_multiplier = 5
-    )
-
-    # formula, assumptions = OnlineInfoPassEncoder().encode(
-    #     new_classes, in_pair, reduced_encoding_class, pseudoboolean_signal_encoder, False, False, True,
-    #     formula, mapp, cmapp, assumptions, known_info
-    # )
-
-    # formula, assumptions = ReducedPseudobooleanEncoder().encode(
-    #     new_classes, in_pair, False, False, True, formula, mapp, cmapp, assumptions, known_info
-    # )
-
-    solver = Solver(name='cadical195', bootstrap_with=formula)
-
-    encoding = time.time()
-
-    print(len(formula.clauses), get_absmax_lit(formula.clauses), "                                                                                                              ")
-
-    print("encoding time: ",encoding - post_new_classes)
-
-    result = solver.solve(assumptions)
-
-    solving = time.time()
-
-    print("solving time: ",solving - encoding)
-    print("max value:", mapp.curr.val)
-
-    print(result)
+            print("Testing: ", file + optimisation, encoder_name, "seed = ", seed)
+            run_affirmative_test(
+                "r1cs_files/" + file + optimisation + ".r1cs",
+                "test_results/" + encoder_name + "/" + file + optimisation + ".json",
+                seed,
+                None,
+                naive_removal_clustering,
+                groups_from_clusters,
+                None,
+                encoder,
+                class_encoding = reduced_encoding_class,
+                signal_encoding = pseudoboolean_signal_encoder,
+                debug=True
+            )
 
 
 
