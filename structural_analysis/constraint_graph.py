975a1506cbd9c8379638922dcb81fed8152d495d
insertrandomcode <paramihijoalejandro@gmail.com>
2024-08-06 18:41:25 +0200

    vastly improved graph construction time

--- structural_analysis/constraint_graph.py
+++ structural_analysis/constraint_graph.py
@@ -1,9 +1,12 @@
 
 from typing import List
 import networkx as nx
+from itertools import combinations
 
 from r1cs_scripts.circuit_representation import Circuit
 from r1cs_scripts.constraint import Constraint
+
+from structural_analysis.graph_clustering.degree_clustering import _signal_data_from_cons_list
 
 def getvars(con: Constraint) -> set:
     return set(con.A.keys()).union(con.B.keys()).union(con.C.keys()).difference(set([0]))
@@ -11,16 +14,15 @@ def getvars(con: Constraint) -> set:
 def shared_signal_graph(cons: List[Circuit]) -> nx.Graph:
 
     graph = nx.Graph()
-    for i, con in enumerate( cons ):
+    _, signal_to_coni = _signal_data_from_cons_list(cons)
 
-        vars = getvars(con)
+    for signal in signal_to_coni.keys():
 
-        for j, ocon in enumerate( cons ):
+        if len(signal_to_coni[signal]) == 1:
+            graph.add_node(next(iter(signal_to_coni[signal])))
+            continue
 
-            if i == j:
-                continue 
-
-            if len( vars.intersection( getvars(ocon)) ) > 0:
-                graph.add_edge(i, j)
+        for i, j in combinations(signal_to_coni[signal], r = 2):
+            graph.add_edge(i, j)
 
     return graph
