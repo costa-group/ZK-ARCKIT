36613dcb8a918edaa7c9294bff024cba9d4db04f
insertrandomcode <paramihijoalejandro@gmail.com>
2024-08-21 17:19:38 +0200

    changed getvars to be faster

--- utilities.py
+++ utilities.py
@@ -1,8 +1,9 @@
 from r1cs_scripts.constraint import Constraint
 from typing import Iterable, Dict
+from itertools import chain
 
 def getvars(con: Constraint) -> set:
-    return set(con.A.keys()).union(con.B.keys()).union(con.C.keys()).difference(set([0]))
+    return set(filter(lambda x : x != 0, chain(con.A.keys(), con.B.keys(), con.C.keys())))
 
 def count_ints(lints : Iterable[int]) -> Dict[int, int]:
     res = {}
