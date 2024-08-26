ebb976e43a15d5285dc0fbed231557c0b5d569ba
insertrandomcode <paramihijoalejandro@gmail.com>
2024-08-26 19:18:08 +0200

    modified call to include names so no hardcoding; changed oset to only check potentially viable signal pairs

--- bij_encodings/single_cons_options.py
+++ bij_encodings/single_cons_options.py
@@ -1,25 +1,25 @@
 from functools import reduce
 from itertools import chain
-from typing import Dict, Set
+from typing import Dict, List, Tuple
 
 from r1cs_scripts.constraint import Constraint
 from bij_encodings.assignment import Assignment
+from utilities import getvars
 
-def signal_options(C1: Constraint, C2: Constraint, mapp: Assignment, 
-                   assumptions: Set[int] = None,
+def signal_options(in_pair: List[Tuple[str, Constraint]], mapp: Assignment,
                    signal_bijection: Dict[str, Dict[int, int]] = None) -> dict:
+    ## TODO: this is the holdup now.
+    # problems come from constraints with 257 different variables, basically all of them with 1.. this causes a lot of assignment calls
+
+
     ## Assume input constraints are in a comparable canonical form
 
     # iterator for dicts in a constraint
     dicts = [ 
-        [d.A, d.B, d.C] for d in [C1, C2]
+        [d.A, d.B, d.C] for _, d in in_pair
     ]
 
-
-    allkeys = [
-        set(filter( lambda key : key != 0, chain(d.A.keys(), d.B.keys(), d.C.keys()))) # not looking to map constants
-        for d in [C1, C2] 
-    ]
+    allkeys = [getvars(d) for _, d in in_pair]
 
     # inv[Ci][part][value] = set({keys in Ci with value in Ci.part})
     inv = [
@@ -41,7 +41,7 @@ def signal_options(C1: Constraint, C2: Constraint, mapp: Assignment,
             for key in dict_.keys():
                 if key == 0:
                     continue
-                inv[i][j].setdefault(dict_[key], set([])).add(key)
+                inv[i][j].setdefault(dict_[key], []).append(key)
                 app[i].setdefault(key, []).append( j )
     
     for j in range(3):
@@ -53,29 +53,32 @@ def signal_options(C1: Constraint, C2: Constraint, mapp: Assignment,
                 for i, name in enumerate(["S1", "S2"])
             }
 
-    # TODO: update so it's not in two steps
-    def get_options_set(name, i, key, update):
-        return set(map(
-                    lambda pair : mapp.get_assignment(*( (key, pair) if name == "S1" else (pair, key) ), update = update),
-                    reduce(
-                        lambda x, y : x.intersection(y),
-                        [ inv[1-i][j][dicts[i][j][key]] for j in app[i][key] ], 
-                        allkeys[1-i]
-                    ) 
-                ))
-
     options = {
-        name: {
-            key: 
-                get_options_set(name, i, key, update = True)
-                if signal_bijection is None or key not in signal_bijection[name].keys() else
-                signal_bijection[name][key].intersection(
-                    get_options_set(name, i, key, update = False)
-                )
-            
-            for key in allkeys[i]
-        }
-        for name, i in [('S1', 0), ('S2', 1)]
+        name: {}
+        for name, _ in in_pair
     }
+
+    for i, (name, _) in enumerate(in_pair):
+        for key in allkeys[i]:
+            oset = reduce(
+                lambda x, y : x.intersection(y),
+                [ inv[1-i][j][dicts[i][j][key]] for j in app[i][key] ], 
+                allkeys[1-i]
+            )
+
+            oset = set(filter(
+                lambda osig : signal_bijection is None or 
+                    osig not in signal_bijection[in_pair[1-i][0]].keys() or 
+                    key in map(lambda ass : mapp.get_inv_assignment(ass)[i], signal_bijection[in_pair[1-i][0]][osig]),
+                oset
+            ))
+
+            if signal_bijection is not None and key in signal_bijection[name].keys():
+                oset.intersection_update(map(lambda ass : mapp.get_inv_assignment(ass)[1-i], signal_bijection[name][key]))
+
+            options[name][key] = set(map(
+                lambda pair : mapp.get_assignment(*((key, pair) if i == 0 else (pair, key))),
+                oset
+            ))
    
     return options
