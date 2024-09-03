2e5113c110a2563a27518d48d20f9e9175b5235c
insertrandomcode <paramihijoalejandro@gmail.com>
2024-09-03 12:54:14 +0200

    reintroduced offset / non-compatible with link

--- bij_encodings/assignment.py
+++ bij_encodings/assignment.py
@@ -5,15 +5,19 @@ class SharedInt():
           self.val = val
 
 class Assignment():
-        def __init__(self, assignees: int = 2, link: "Assignment" = None):
+        def __init__(self, assignees: int = 2, link: "Assignment" = None, offset: int = 0):
             self.assignment = {}
             self.inv_assignment = [None]
             self.curr = SharedInt(1)
             self.assignees = assignees
+            self.offset = offset
 
             if link is not None:
                 self.inv_assignment = link.inv_assignment
                 self.curr = link.curr
+
+                if self.offset != 0:
+                    raise ValueError("Linked Assignments with offset not available")
         
         def get_assignment(self, *args, update: bool = True) -> int:
             ## assignment i, j is from S1 to S2
@@ -32,7 +36,7 @@ class Assignment():
                     del curr[args[-1]]
                     return None
                 # set value
-                curr[args[-1]] = self.curr.val
+                curr[args[-1]] = self.curr.val + self.offset
                 self.inv_assignment.append(args)
                 self.curr.val += 1
                 return curr[args[-1]]
@@ -41,5 +45,5 @@ class Assignment():
                 return res
         
         def get_inv_assignment(self, i: int) -> Tuple[int, int]:
-            assert i > 0, "invalid choice for i"
-            return self.inv_assignment[i]
+            assert i > self.offset, "invalid choice for i"
+            return self.inv_assignment[i - self.offset]
