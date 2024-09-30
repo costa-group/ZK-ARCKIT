066477b2c0d9f5a39a39c6719aab299c303e33f1
insertrandomcode <paramihijoalejandro@gmail.com>
2024-09-30 15:40:20 +0200

    first implementation of extended link clustering

---
+++ structural_analysis/clustering_methods/linear_coefficient.py
@@ -0,0 +1,92 @@
+from typing import Iterable, List, Tuple
+import itertools
+
+from utilities import getvars, _signal_data_from_cons_list, UnionFind
+
+from r1cs_scripts.circuit_representation import Circuit
+from r1cs_scripts.constraint import Constraint
+
+from structural_analysis.clustering_methods.naive.clustering_from_list import cluster_by_ignore, IgnoreMethod
+
+def cluster_by_linear_coefficient(circ: Circuit, coefs: Iterable[int] = [1, -1], **clustering_kwargs):
+    coefs = list(map(lambda x : x % circ.prime_number, coefs))
+
+    """
+    step 1: split naively as before (w/ constant)
+
+    step 2: find candiate signal/constraint pairs
+        - must have coef in coefs in the C part of the constraint
+        - must not be previous
+
+    step 3: for each candidate pair, check if internal signal path is reachable through only noncandidates
+        - if yes, remove candidate - if no candidates for constraint make constraint noncandidate
+        - if no, continue
+    
+    step 4: split on candidates too
+    
+    """
+    # Step 1
+
+    def _assumed_link_constraint(con: Constraint) -> bool:
+        if not (len(con.A) == len(con.B) == 0): return False
+        vars = getvars(con)
+        if len(vars) != 2 or any(map(lambda sig : con.C[sig] not in coefs, vars)): return False
+        return True
+    
+    removed, leftovers = [], []
+    for coni, con in enumerate(circ.constraints): (removed if _assumed_link_constraint(con) else leftovers).append(coni)
+    
+    leftovers = set(range(circ.nConstraints)).difference(removed)
+    noncandidates, candidates = [], []
+
+    def _is_candidate_constraint(coni: int) -> bool:
+        # all sig, value are potential candidates for con not in removed
+        for sig, value in filter(lambda tup: tup[0] != 0 and tup[1] in coefs, circ.constraints[coni].C.items()):
+            return True
+        return False 
+    
+    for coni in leftovers: (candidates if _is_candidate_constraint(coni) else noncandidates).append(coni)
+    
+    noncandidate_uf = UnionFind()
+    for coni in noncandidates: noncandidate_uf.union(*getvars(circ.constraints[coni]))
+
+    def _get_candidate_pairs(coni: int) -> List[Tuple[int, int]]:
+        return list(itertools.product([coni],
+            filter(lambda sig : sig != 0 and circ.constraints[coni].C[sig] in coefs, circ.constraints[coni].C.keys())))
+
+    # if taking too long refactor so only loops once
+    signal_to_candidate_coni = {}
+    coni_to_num_candidates = {}
+    pipe = []
+
+    for coni, sig in itertools.chain(*map(_get_candidate_pairs, candidates)):
+        signal_to_candidate_coni.setdefault(sig, set([])).add(coni) # slow..
+        coni_to_num_candidates[coni] = coni_to_num_candidates.setdefault(coni, 0) + 1
+        pipe.append((coni, sig))
+
+    i = 0
+    while len(pipe) > 0:
+        i += 1
+        print(i, end='\r')
+
+        # TODO: try not to double check so much
+        coni, sig = pipe.pop()
+
+        if any(map(lambda osig : sig != osig and noncandidate_uf.find(sig) == noncandidate_uf.find(osig), getvars(circ.constraints[coni]))):
+            coni_to_num_candidates[coni] -= 1
+            signal_to_candidate_coni[sig].remove(coni)
+
+            if len(signal_to_candidate_coni[sig]) == 0: del signal_to_candidate_coni[sig]
+
+            if coni_to_num_candidates[coni] == 0:
+                noncandidate_uf.union(*getvars(circ.constraints[coni]))
+
+                # check any candidate pairs that have been merged
+                pipe.extend(itertools.chain(
+                    *map( lambda osig : itertools.product(signal_to_candidate_coni[osig], [osig]),
+                    filter(lambda osig : noncandidate_uf.find(sig) == noncandidate_uf.find(osig), signal_to_candidate_coni.keys())
+                    )))
+
+    links = list(filter(lambda coni : coni_to_num_candidates[coni] > 0, coni_to_num_candidates.keys()))
+    print("")
+    return cluster_by_ignore(circ, IgnoreMethod.ignore_constraint_from_list, removed + links, **clustering_kwargs)
