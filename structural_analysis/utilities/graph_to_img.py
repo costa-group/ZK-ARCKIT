3eba3272a8bbb49f32dc950736bc375ba19be4a0
insertrandomcode <paramihijoalejandro@gmail.com>
2024-10-29 18:18:54 +0100

    first handling of dag ordering

--- structural_analysis/utilities/graph_to_img.py
+++ structural_analysis/utilities/graph_to_img.py
@@ -1,6 +1,7 @@
-from typing import List
+from typing import List, Tuple
 import networkx as nx
 import pydot as pd
+import itertools
 
 from utilities import getvars
 from r1cs_scripts.circuit_representation import Circuit
@@ -24,7 +25,7 @@ def circuit_graph_to_img(circ: Circuit, G: nx.Graph) -> pd.Graph:
     
     return g
 
-def partition_graph_to_img(circ: Circuit, G: nx.Graph, partition: List[List[int]], outfile: str = "test.png"):
+def partition_graph_to_img(circ: Circuit, G: nx.Graph, partition: List[List[int]], outfile: str = "test.png", return_graph: bool = False) -> pd.Graph:
     g = circuit_graph_to_img(circ, G)
 
     for i, part in enumerate(partition):
@@ -33,4 +34,42 @@ def partition_graph_to_img(circ: Circuit, G: nx.Graph, partition: List[List[int]
     
         g.add_subgraph(c)
     
+    if return_graph: return g
     g.write_png(outfile)
+
+def dag_graph_to_img(circ: Circuit, G: nx.Graph, partition: List[List[int]], arcs: List[Tuple[int, int]], outfile: str = "test.png"):
+
+    g = partition_graph_to_img(circ, G, partition, return_graph = True)
+    g.set_type("digraph")
+
+    coni_to_part = [None for _ in range(circ.nConstraints)]
+    for i, part in enumerate(partition):
+        for coni in part: coni_to_part[coni] = i
+
+    partition_outgoing = [ [] for part in partition ]
+
+    for edge in g.get_edge_list(): partition_outgoing[coni_to_part[int(edge.get_source())]].append(edge)
+
+    print(partition_outgoing)
+
+    for arc in arcs:
+        parti, partj = arc
+
+        edges_between = itertools.chain(
+            filter(lambda edge : partj == coni_to_part[int(edge.get_destination())], partition_outgoing[parti]),
+            filter(lambda edge : parti == coni_to_part[int(edge.get_destination())], partition_outgoing[partj])
+        )
+        edges_between = list(edges_between)
+
+        for edge in edges_between:
+            src, dst = edge.get_source(), edge.get_destination()
+            print(src, dst)
+            if int(src) in partition[parti]: continue
+            print("swapped")
+            g.del_edge(src, dst)
+            g.add_edge(pd.Edge(dst, src))
+
+    g.write_png(outfile)
+        
+
+    
