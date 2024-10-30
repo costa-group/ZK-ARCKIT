150ef30079403685e1a3db2495060f617aa14c50
insertrandomcode <paramihijoalejandro@gmail.com>
2024-10-30 14:23:49 +0100

    fixed a bug where a constraint was appearing in multiple parts

--- structural_analysis/clustering_methods/nonlinear_attract.py
+++ structural_analysis/clustering_methods/nonlinear_attract.py
@@ -1,4 +1,4 @@
-from typing import Iterable, List, Tuple
+from typing import Iterable, List, Tuple, Dict, Set
 import itertools
 
 from utilities import UnionFind, _signal_data_from_cons_list, getvars
@@ -17,53 +17,54 @@ def nonlinear_attract_clustering(circ: Circuit):
     """
 
     sig_to_coni = _signal_data_from_cons_list(circ.constraints)
-    coni_to_adjacent_coni = lambda coni : set(itertools.chain(*map(sig_to_coni.__getitem__, getvars(circ.constraints[coni])))).difference([coni])
+    coni_to_adjacent_coni = lambda coni : set(filter(lambda oconi: oconi != coni, itertools.chain(*map(sig_to_coni.__getitem__, getvars(circ.constraints[coni])))))
 
     def _is_nonlinear(con: Constraint) -> bool:
         return len(con.A) > 0 and len(con.B) > 0
     
     nonlinear_clusters = UnionFind()
 
-    for coni, _ in filter(lambda tup : _is_nonlinear(tup[1]), enumerate(circ.constraints)):
+    for coni in filter(lambda coni : _is_nonlinear(circ.constraints[coni]), range(circ.nConstraints)):
         nonlinear_clusters.union(coni, *filter(lambda oconi : _is_nonlinear(circ.constraints[oconi]), coni_to_adjacent_coni(coni)) )
     
     clusters = {}
     for coni in nonlinear_clusters.parent.keys():
         clusters.setdefault(nonlinear_clusters.find(coni), []).append(coni)
     
-    adj_coni = {}
+    adjacent_nonlinear = {}
     for key, cluster in clusters.items():
-        adj_coni[key] = set(itertools.chain(
-            *map(
-                lambda coni : filter(lambda oconi : nonlinear_clusters.find(oconi) == oconi, coni_to_adjacent_coni(coni)),
-                cluster
-            )
-        ))
+            adjacent_nonlinear[key] = set(
+                itertools.chain(
+                *map(
+                    lambda coni : filter(lambda oconi : not _is_nonlinear(circ.constraints[oconi]) and 
+                        nonlinear_clusters.find(oconi) == oconi, coni_to_adjacent_coni(coni)),
+                    cluster
+                )
+            ))
     
     updated = True
+
     while updated:
 
         adjacent_to = {}
 
-        for key, adjacencies in adj_coni.items():
+        for key, adjacencies in adjacent_nonlinear.items():
             for coni in adjacencies:
                 adjacent_to.setdefault(coni, []).append(key)
-        
-        adj_coni = {}
-
-        for key in adj_coni.keys():
-            adj_coni[key].difference_update(adjacent_to.keys())
 
         to_attract = list(filter(lambda key : len(adjacent_to[key]) == 1, adjacent_to.keys()))
         updated = len(to_attract) > 0
 
+        adjacent_nonlinear = {}
+
         # need to do this first for consistency
         for coni in to_attract:
-            nonlinear_clusters.union(coni, adjacent_to[coni][0])
-        
+            nonlinear_clusters.parent[coni] = adjacent_to[coni][0]
+
         for coni in to_attract:
-            clusters[adjacent_to[coni][0]].append(coni)
-            adj_coni[adjacent_to[coni][0]] = set(filter(lambda oconi : nonlinear_clusters.find(oconi) == oconi, coni_to_adjacent_coni(coni)))
-    
+            key = adjacent_to[coni][0]
+            clusters[key].append(coni)
+            adjacent_nonlinear.setdefault(key, set([])).update(filter(lambda oconi : not _is_nonlinear(circ.constraints[oconi]) and nonlinear_clusters.find(oconi) == oconi, coni_to_adjacent_coni(coni)))
+
     return clusters, None, None
 
