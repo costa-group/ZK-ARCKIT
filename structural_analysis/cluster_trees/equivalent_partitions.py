a1c034fb18bbdcdf69f13cabcebcd2d6ed74331c
insertrandomcode <paramihijoalejandro@gmail.com>
2024-11-06 19:50:58 +0100

    implemented easy fingerprinting and brainstormed some true multi-compare

--- structural_analysis/cluster_trees/equivalent_partitions.py
+++ structural_analysis/cluster_trees/equivalent_partitions.py
@@ -1,10 +1,12 @@
 from typing import List, Dict
 import itertools
+import collections
 
 from structural_analysis.cluster_trees.dag_from_clusters import DAGNode
 from r1cs_scripts.circuit_representation import Circuit
 from r1cs_scripts.constraint import Constraint
 from utilities import getvars
+from bij_encodings.assignment import Assignment
 from testing_harness import quick_compare
 
 def naive_equivalency_analysis(nodes: Dict[int, DAGNode], time_limit: int = 0) -> List[List[int]]:
@@ -36,13 +38,40 @@ def naive_equivalency_analysis(nodes: Dict[int, DAGNode], time_limit: int = 0) -
     
     return classes
 
-    
+def easy_fingerprint_then_equivalence(nodes: Dict[int, DAGNode], time_limit: int = 0) -> List[List[int]]:
+    """
+    fingerprints ensuring all groups have the same number of constraints, signals, inputs/outputs
 
+    """
+    NKAssignment = Assignment(assignees=1)
+    NKGroups = {}
 
-def collective_equivalency_classes(circ: Circuit, nodes: List[Dict]) -> List[List[int]]:
+    for node, key in zip(nodes.values(), 
+        map(lambda circ: (circ.nConstraints, circ.nWires, circ.nPrvIn + circ.nPubIn, circ.nPubOut), 
+        map(lambda node : node.get_subcircuit(), nodes.values()))):
+
+        hash_ = NKAssignment.get_assignment(key)
+        NKGroups.setdefault(hash_, {})[node.id] = node
+
+    return list(itertools.chain(*map(
+            lambda nodes_subset : naive_equivalency_analysis(nodes_subset, time_limit), 
+            NKGroups.values()
+        )))
+
+def collective_equivalency_classes(circ: Circuit, nodes: List[DAGNode]) -> List[List[int]]:
     """
     does each step collectively hopefully reducing time
+
+    can fingerprint all constraints, then create n-tuples for each fingerprint -- no guarantee of nonemptiness now
+        - by transitivity of enclosure, we test equivalence only to first circuit, 
+            -- doing that is still n^2 (* compare) in the worst case right?
+        - we can do an all-to-all comparison for each class but that seems extremely expensive...
+    
+    Extending this is actually a lot of effort, since none of the information passing stuff works here 
+        (in addition to refactoring all the various part of the comparison algorithm)
+
+    If we assume that naive equivalency is n^2 in a group we can first fingerprint them to save some time.
+    If we want to eventually call naive equivalency then we are gonna want to the fingerprint to be easy
+    If we do constraint level fingerprinting we really ought to design an entirely new algorithm
     """
-
-
-    pass # TODO
+    pass #TODO
