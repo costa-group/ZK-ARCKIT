dde8f3bf7dafe1bbfc5e4b4e647abfe07dab207b
insertrandomcode <paramihijoalejandro@gmail.com>
2025-04-21 14:50:12 +1000

    checkpoint testing commit

--- clustering_and_equivalence (stage 1)/testing.py
+++ clustering_and_equivalence (stage 1)/testing.py
@@ -36,9 +36,8 @@ from normalisation import r1cs_norm
 from utilities import count_ints, _signal_data_from_cons_list, getvars
 from comparison.static_distance_preprocessing import _distances_to_signal_set
 from maximal_equivalence.shortest_minimum_span import shortest_minimum_span
-from maximal_equivalence.maximal_equivalence import maximum_equivalence
+from maximal_equivalence.applied_maximal_equivalence import maximally_equivalent_classes
 from structural_analysis.cluster_trees.dag_from_clusters import DAGNode
-from maximal_equivalence.applied_maximal_equivalence import maximally_equivalent_classes, pairwise_maximally_equivalent_classes
 import itertools
 
 def cons_eq(C1: Constraint, C2: Constraint):
@@ -55,18 +54,16 @@ def cons_eq(C1: Constraint, C2: Constraint):
 def get_absmax_lit(clauses):
     return max(map(max, map(lambda x : map(abs, x), clauses)))
 
-def to_dagnode(circ, node):
-    return DAGNode(circ, node["node_id"], node["constraints"], set(node["input_signals"]), set(node["output_signals"]))
     
 if __name__ == '__main__':
 
-    filenames = ["binsub_test", "Poseidon", "Reveal", "Biomebase", "Move", "sha256", "test_ecdsa", "test_ecdsa_verify"]
+    filenames = ["Poseidon", "Reveal", "Biomebase", "Move", "sha256", "test_ecdsa", "test_ecdsa_verify"]
     compilers = ["O0", "O1"]
 
     clustering = "louvain"
     test_dir = "with_mapping"
 
-    for name, comp in itertools.product(filenames[:-1], compilers[:]):
+    for name, comp in itertools.product(filenames[1:-1], compilers):
 
         print(f"########################### {name} {comp} ###########################")
 
@@ -79,49 +76,19 @@ if __name__ == '__main__':
             fp = open(jsonfile, 'r')
             clusters = json.load(fp)
             fp.close()
-        except FileNotFoundError as e:
-            print(e)
+        except FileNotFoundError:
             continue
 
+        print(len(clusters["nodes"]))
         nodes = clusters["nodes"]
-        dagnodes = { node["node_id"] : to_dagnode(circ, node) for node in nodes}
-        equivalency = list(map(lambda lst : lst[0], clusters["equivalency"]))
 
-        added_inputs = False
-        added_output = False
-        for node, dagnode in zip(nodes, dagnodes.values()):
-            if not set(filter(lambda sig : 0 < sig <= circ.nPubOut, node["signals"])).issubset(dagnode.output_signals): added_output = True
-            if not set(filter(lambda sig : circ.nPubOut < sig <= circ.nPubOut + circ.nPrvIn + circ.nPubIn, node["signals"])).issubset(dagnode.input_signals): added_inputs = True
+        def node_to_dagnode(node):
+            return DAGNode(circ, node["node_id"], node["constraints"], set(node["input_signals"]), set(node["output_signals"]))
 
-            dagnode.input_signals.update(filter(lambda sig : circ.nPubOut < sig <= circ.nPubOut + circ.nPrvIn + circ.nPubIn, node["signals"]))
-            dagnode.output_signals.update(filter(lambda sig : 0 < sig <= circ.nPubOut, node["signals"]))
-            # assert set(filter(lambda sig : 0 < sig <= circ.nPubOut, node["signals"])).issubset(dagnode.output_signals), f"{dagnode.output_signals}, {list(filter(lambda sig : 0 < sig <= circ.nPubOut, node['signals']))}"
-            # assert set(filter(lambda sig : circ.nPubOut < sig <= circ.nPubOut + circ.nPrvIn + circ.nPubIn, node["signals"])).issubset(dagnode.input_signals), f"{dagnode.output_signals}, {list(filter(lambda sig : circ.nPubOut < sig <= circ.nPubOut + circ.nPrvIn + circ.nPubIn, node['signals']))}"
-        print(f"Had to fix outputs: {added_output}, and inputs: {added_inputs}")
-
-
-        start = time.time()
-        # names = ["S1", "S2"]
-        # circ = [dagnodes[38].get_subcircuit(), dagnodes[48].get_subcircuit()]
-        # in_pair = list(zip(names, circ))
-        # coni_pairs, _ = maximum_equivalence(in_pair, debug=True, solver_timeout = 5)
-        # print(len(coni_pairs), circ[0].nConstraints)
-
-        # indices = [38, 48, 73, 83, 103, 113]
-        # res = pairwise_maximally_equivalent_classes({ ind : dagnodes[ind] for ind in indices}, tol=0.8, solver_timeout=5)
-        # print(res)
-
-        # print(len(clusters["equivalency"]), count_ints(map(len, clusters["equivalency"])))
-        # classes = maximally_equivalent_classes(dagnodes, tol=0.80, solver_timeout = 5, exit_max_classes=True)
-        # print(classes)
-
-        # print(time.time() - start)
-
-        # start = time.time()
-        # classes = maximally_equivalent_classes(dagnodes, clusters["equivalency"], clusters["equiv_mappings"], tol=0.80, solver_timeout = 5, exit_max_classes=True)
-        # print(classes)
+        dagnodes = { node["node_id"] : node_to_dagnode(node) for node in nodes }
         
-        nodes, equivalency = maximally_equivalent_classes(dagnodes, clusters["equivalency"], clusters["equiv_mappings"], solver_timeout = 5)
-
-        print(time.time() - start)
+        results = maximally_equivalent_classes(dagnodes, clusters["equivalency"], clusters["equiv_mappings"], tol=0.8, solver_timeout=5)
         
+        fp = open(f"clustering_tests/{test_dir}/{name}{comp}_{clustering}_maxequiv.json", 'w')
+        json.dump(results, fp, indent=4)
+        fp.close()
