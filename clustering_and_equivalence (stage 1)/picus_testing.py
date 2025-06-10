60f6e548138ec232f7ad34f6db6d85ada2674072
Alejandro Stuckey de la Banda <paramihijoalejandro@gmail.com>
2025-06-10 18:37:54 +1000

    added explanation and fixed bug

--- clustering_and_equivalence (stage 1)/picus_testing.py
+++ clustering_and_equivalence (stage 1)/picus_testing.py
@@ -10,26 +10,42 @@ from r1cs_scripts.circuit_representation import Circuit
 from structural_analysis.cluster_trees.dag_from_clusters import DAGNode
 
 if __name__ == '__main__':
+    ######################################################
+    ### CAMBIA LO SEGUIDE PARA QUE HAGA LO QUE QUIERES ###
+    ######################################################
+
+    # the list of files and compilers -- invalid are skipped
     filenames = ["Poseidon", "Reveal", "Move", "Biomebase", "sha256_test512", "test_ecdsa", "circuit-1-2-1-8_"]
     compilers = ["O0", "O1"]
 
-    ## get json files and load .r1cs
-    r1cs = "RevealO0"
+    testdir = "clustering_tests/picusbenchmarking"
     
+    ##
+    # looks for files of {testdir}/{filename}{compiler}_{clustering}_{equivalence}{suffixes} 
+    ##
+    # cambia lo de abajo si necesitas clustering/equivalence/suffixes differentes
     clustering = "louvain"
     equivalence = "structural"
     suffixes = "_maxequiv"
 
-    testdir = "picusbenchmarking"
+    ## Te dara en un archivo dentro de testdir que se llama r1cs
+    #     dentro habra archivos con nombres que dentro tendran 0.r1cs, 0.sym, 1.r1cs, 1.sym, etc..
+    #     cada uno es un cluster de cada classe de equivalence (estructural si puede)
 
+    ######################################################
+    ################## AQUI NO CAMBIAR ###################
+    ######################################################
+
+    dir_exists = os.path.exists(testdir + "/r1cs/")
+    if not dir_exists: os.mkdir(testdir + "/r1cs/")
     for file, comp in itertools.product(filenames[:], compilers[:]):
         print(file, comp)
 
         r1cs = file + comp
 
-        jsonfile = f"clustering_tests/{testdir}/{r1cs}_{clustering}_{equivalence}{suffixes}.json"
+        jsonfile = f"{testdir}/{r1cs}_{clustering}_{equivalence}{suffixes}.json"
         r1csfile = f"r1cs_files/{r1cs}.r1cs"
-        output_directory = f"clustering_tests/{testdir}/r1cs/{r1cs}_{clustering}_{equivalence}{suffixes}/"
+        output_directory = f"{testdir}/r1cs/{r1cs}_{clustering}_{equivalence}{suffixes}/"
 
         try:
             circ = Circuit()
@@ -56,7 +72,7 @@ if __name__ == '__main__':
         ## Circuit to .r1cs files
         dir_exists = os.path.exists(output_directory)
         if not dir_exists: os.mkdir(output_directory)
-        for class_ in jsondata[f"equivalency_{equivalence}"]:
+        for class_ in jsondata["equivalence_local" if equivalence == 'local' else "equivalency_structural"]:
             id0 = class_[0]
             write_r1cs(subcircs[id0], output_directory + str(id0) + ".r1cs", sym=True)
 
