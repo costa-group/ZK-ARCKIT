ba9df58181e0b22e86473f9727f8f7a6d0bffda3
Alejandro Stuckey de la Banda <paramihijoalejandro@gmail.com>
2025-10-20 17:38:07 +1100

    moved shared_constraint_graph to constraint_graph.py and changed both to use igraph over networkx

--- clustering_and_equivalence (stage 1)/structural_analysis/utilities/constraint_graph.py
+++ clustering_and_equivalence (stage 1)/structural_analysis/utilities/constraint_graph.py
@@ -1,45 +1,56 @@
 
-from typing import List
-import networkx as nx
+from typing import List, Tuple, Dict
+import igraph as ig
 from itertools import combinations
 
 from circuits_and_constraints.abstract_constraint import Constraint
 from utilities.utilities import _signal_data_from_cons_list
 
-def shared_signal_graph(cons: List[Constraint], names: List[int] | None = None) -> nx.Graph:
+def shared_signal_graph(cons: List[Constraint]) -> ig.Graph:
     """
-    Given an input list of constraints, returns a networkx graph.
+    Given an input list of constraints, returns a igraph graph.
     Vertices in the graph are constraint, edges are between constraints with a shared non-constant signal
 
     Parameters
     ----------
         cons: List[Constraint]
             List of constraints passed to :func:`_signal_data_from_cons_list`
-        names: List[int] | None
-            Indices for constraints passed to :func:`_signal_data_from_cons_list`
-    
     Returns
     ----------
-    nx.Graph
+    ig.Graph
         Vertices in the graph are constraint, edges are between constraints with a shared non-constant signal
     """
 
-    graph = nx.Graph()
-    signal_to_coni = _signal_data_from_cons_list(cons, names)
+    graph = ig.Graph()
+    signal_to_coni = _signal_data_from_cons_list(cons)
 
     weights = {}
 
-    for signal in signal_to_coni.keys():
+    graph.add_vertices(len(cons))
+    graph.add_edges
 
-        if len(signal_to_coni[signal]) == 1:
-            graph.add_node(next(iter(signal_to_coni[signal])))
-            continue
+    for signal in signal_to_coni.keys():
 
         for i, j in combinations(signal_to_coni[signal], r = 2):
             i, j = min(i,j), max(i,j)
-            weights[(i, j)] = weights.setdefault((i, j), 0) + 1
+            weights[(i, j)] = weights.get((i, j), 0) + 1
 
-    for i, j in weights.keys():
-        graph.add_edge(i, j, weight = weights[(i, j)])
+    graph.add_edges([(i, j) for i, j in weights.keys()], attributes={"weight": weights.values()})
 
+    return graph
+
+def shared_constraint_graph( cons: List[Constraint] ) -> ig.Graph:
+    
+    graph = ig.Graph()
+
+    edges = set([])
+
+    for con in cons:
+
+        signals = con.signals()
+
+        graph.add_vertices(map(str, signals)) ## dealing with non-comprehensive signals that can be quite common...
+        edges.update(combinations(signals, r=2))
+    
+    graph.add_edges(edges)
     return graph
